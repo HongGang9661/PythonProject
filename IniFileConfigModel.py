49b81af72170edbd4d6039325acd01cbe69edca8
honggang_li <honggang_li@kingdee.com>
2021-12-15 18:30:34 +0800

    .ini配置文件_python模块configparser的使用

---
+++ IniFileConfigModel.py
@@ -0,0 +1,26 @@
+import configparser
+
+# 创建管理对象
+config = configparser.ConfigParser()
+
+# 读取.ini文件
+file = "config.ini"
+config.read(file, "utf-8")
+
+# 获取sectings、items、otptions、valua
+config.sections()
+config.items(section="")
+
+config.options(section="")
+config.get(section="", option="")
+
+# 判断section、option是否存在
+config.has_section("")
+config.has_option(section="")
+
+# 移除section、option
+config.remove_section(section="")
+config.remove_option(section="", option="")
+
+# 添加section、option、value
+config.set(section="", option="", value="")
