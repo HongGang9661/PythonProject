9804b1715b49d3a4fe948f917c9649c541fb7307
MuZi <honggang_li@icloud.com>
2021-12-18 17:32:41 +0800

    configparser模块函数的补充

--- IniFileConfigModel.py
+++ IniFileConfigModel.py
@@ -9,8 +9,8 @@ import configparser
 config = configparser.ConfigParser()
 
 # 读取.ini文件
-file = "config.ini"
-config.read(file, "utf-8")
+file_ini = "IniFile.ini"
+config.read(file_ini, "utf-8")
 
 # 获取sectings、items、otptions、valua
 config.sections()
@@ -28,4 +28,13 @@ config.remove_section(section="")
 config.remove_option(section="", option="")
 
 # 添加section、option、value
+config.add_section(section="")
 config.set(section="", option="", value="")
+
+# 上面的方法并没有真正的修改ini文件内容，只有当执行conf.write()方法的时，才会修改文件里的内容
+config.write(open(file_ini), 'a')
+
+# 使用with open方法写入
+with open(file_ini, 'a') as f:
+    f.seek(2)   # 移动鼠标指针到末尾
+    config.write(f)
