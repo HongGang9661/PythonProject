e949d696eebb02779befd29a10fc3462ecf44a23
MuZi <honggang_li@icloud.com>
2021-12-21 08:52:05 +0800

    补充yaml模块dump函数的使用

--- PythonYamlModel.py
+++ PythonYamlModel.py
@@ -1,16 +1,24 @@
 """关于yaml文件的格式及yaml模块的使用"""
 
+import os
 import yaml
 
 
 def read_yaml(filedir):
-    with open(filedir, encoding='utf-8') as f:
+    with open(filedir, 'r', encoding='utf-8') as f:
         date_yaml = yaml.full_load(f)
-        yaml.dump()
         return date_yaml
 
 
+def write_yaml(obj, filedir):
+    with open(filedir, 'w', encoding='utf-8') as f:
+        yaml.dump(obj, f)
+
+
 if __name__ == '__main__':
-    date = read_yaml('')
+    date = read_yaml('YamlFile.yaml')
     print(date)
-    print(type(date))
+    dict_yaml = {'name': "muzi", 'email': 'honggang_li@icloud.com', 'phone': [15071110157, 1234567,987654]}
+    path_dir = os.path.dirname(os.path.realpath('__file__'))
+    path_yaml = os.path.join(path_dir, 'yaml_dump.yaml')
+    write_yaml(dict_yaml, path_yaml)
